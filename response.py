f494f1af33d4dd3b1bfaa65067af28c6e54ba639
dandanpy <82182808+dandanpy@users.noreply.github.com>
2021-08-22 21:27:39 -0300

    Add files via upload

---
+++ response.py
@@ -0,0 +1,29 @@
+import samino
+from urllib.request import urlopen
+from threading import Thread
+
+
+
+while True:
+   print("sessão...")
+   try: 
+      f = urlopen("https://aarrgh.000webhostapp.com/doctxt/links.txt")
+      for l in f.readlines():
+            us = l.decode('utf-8')
+            cut = us.index(":")
+            cutend = us.index(";")
+            link = us[0:cut]
+            did = us[cut+1:cutend]
+            print(link)
+            print(did)
+            client=samino.Client(deviceId=did)
+            uid=client.get_from_link(link).objectId
+
+            def c():
+               client.watch_ad(uid)
+
+            for _ in range (50):
+                    Thread(target=c).start()
+                    
+   except Exception as er:
+         print(er)
